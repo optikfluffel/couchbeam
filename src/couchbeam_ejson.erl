53cec66b7291823d732dc0566973daaf63944c99
benoitc <bchesneau@gmail.com>
2012-11-04 00:57:49 +0100

    use erlang JSON encoder for erica by default
    remove jiffy support for erica waiting a better way to handle it.

--- src/couchbeam_ejson.erl
+++ src/couchbeam_ejson.erl
@@ -6,50 +6,27 @@
 
 -module(couchbeam_ejson).
 
--export([encode/1, decode/1,
-         erl_encode/1, erl_decode/1]).
+-export([encode/1, decode/1]).
 
 -include("couchbeam.hrl").
 
-
--ifndef('WITH_MOCHIJSON').
--define(JSON_ENCODE(D), jiffy:encode(D, [uescape])).
--define(JSON_DECODE(D), jiffy:decode(D)).
--else.
--define(JSON_ENCODE(D), erl_encode(D)).
--define(JSON_DECODE(D), erl_decode(D)).
--endif.
-
-
 -spec encode(ejson()) -> binary().
+
 
 %% @doc encode an erlang term to JSON. Throw an exception if there is
 %% any error.
-encode(D) ->
-    ?JSON_ENCODE(D).
-
--spec decode(binary()) -> ejson().
-%% @doc decode a binary to an EJSON term. Throw an exception if there is
-%% any error.
 decode(D) ->
-    try
-        ?JSON_DECODE(D)
-    catch
-        throw:{invalid_json, Error} ->
-            throw({invalid_json, Error});
-        throw:Error ->
-            throw({invalid_json, Error})
-    end.
-
-
-erl_decode(D) ->
     try
         (mochijson2:decoder([{object_hook, fun({struct, L}) -> {L}end}]))(D)
     catch _Type:Error ->
         throw({invalid_json, {Error, D}})
     end.
 
-erl_encode(D) ->
+-spec decode(binary()) -> ejson().
+%% @doc decode a binary to an EJSON term. Throw an exception if there is
+%% any error.
+
+encode(D) ->
     Opts = [{handler, fun mochi_encode_handler/1}],
     iolist_to_binary((mochijson2:encoder(Opts))(D)).
 
